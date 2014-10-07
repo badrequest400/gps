27557a888423ba8b1a8428322d6d185cc45ab111
Balazs Cseh <balazs.cseh89@gmail.com>
2014-10-07 23:50:40 +0100

    model done, parsing function done, writes to DB as is, datatypes need to be sorted out

--- models.js
+++ models.js
@@ -1,1 +1,18 @@
 var mongoose = require('mongoose');
+
+var logSchema = mongoose.Schema({
+
+	GID: Number,
+	time: Number,
+	lat: Number,
+	lon: Number,
+	speed: Number,
+	orientation: Number,
+	date: Number,		//needs to be changed to real Date
+	input: String,
+	output: String,
+	analogue: String,
+	mystery: Number
+});
+
+module.exports.Log = mongoose.model('Log', logSchema);
